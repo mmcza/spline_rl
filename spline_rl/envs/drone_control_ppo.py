b0e5b50b0544888473b189f3a824cd1977c917ae
Daria Kubacka <126802160+dariak153@users.noreply.github.com>
2024-06-14 10:02:18 +0200

    Update drone_control_ppo.py

--- spline_rl/envs/drone_control_ppo.py
+++ spline_rl/envs/drone_control_ppo.py
@@ -15,7 +15,7 @@ except:
 
 from stable_baselines3 import PPO
 from stable_baselines3.ppo.policies import MlpPolicy
-
+# Define directories for saving models and logs
 models_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "rotorpy", "learning", "policies")
 log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "rotorpy", "learning", "logs")
 if not os.path.exists(models_dir):
@@ -23,13 +23,14 @@ if not os.path.exists(models_dir):
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
-num_cpu = 4
+num_cpu = 4 # Number of CPUs for parallel processing (if applicable)
 
+# Custom environment subclassing the QuadrotorEnv
 class CustomQuadrotorEnv(QuadrotorEnv):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rewarded = False
-
+        self.rewarded = False # Track if reward has been given
+  # Custom reward function
     def compute_reward(self, state, action):
         if np.abs(state[0] - 5.0) < 0.05 and not self.rewarded:
             self.rewarded = True
@@ -47,10 +48,10 @@ class CustomQuadrotorEnv(QuadrotorEnv):
                 else:
                     x = state[0]
                 return -np.exp(5.0 - x) * 0.001 
-
+# Define the world map with boundaries
 world_map = {"bounds": {"extents": [-10., 10., -10., 10., -10, 10.]}}
 world = World(world_map)
-
+# custom environment
 env = CustomQuadrotorEnv(
     control_mode ='cmd_motor_speeds', 
     quad_params = quad_params,
@@ -61,18 +62,19 @@ env = CustomQuadrotorEnv(
 )
 
 env.reward_fn = env.compute_reward  # Set the reward function to the instance method
-
+# Reset the environment with a random initial state
 observation, info = env.reset(initial_state='random', options={'pos_bound': 2, 'vel_bound': 0})
-
+# Create the PPO model
 model = PPO(MlpPolicy, env, verbose=1, ent_coef=0.01, tensorboard_log=log_dir)
 
-num_timesteps = 20_000
-num_epochs = 10
+num_timesteps = 20_000 # Number of timesteps per learning iteration
+num_epochs = 10 # Total number of learning epochs
 
-start_time = datetime.now()
+start_time = datetime.now() # Record the start time
 
-epoch_count = 0
+epoch_count = 0  # Initialize epoch counter
 while True:
+       # Train the model for a specified number of timesteps
     model.learn(total_timesteps=num_timesteps, reset_num_timesteps=False, tb_log_name="PPO-Quad_cmd-motor_"+start_time.strftime('%H-%M-%S'))
     model.save(f"{models_dir}/PPO/{start_time.strftime('%H-%M-%S')}/hover_{num_timesteps*(epoch_count+1)}")
-    epoch_count += 1
+    epoch_count += 1  # Increment the epoch counter
